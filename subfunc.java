fc57e94d966e8d5766b4c8cef1a742dea4babc63
Rajgcp <rprasadp.yxe@gmail.com>
2020-05-31 21:40:10 +0000

    18091809$&&&&&

--- subfunc.java
+++ subfunc.java
@@ -2,8 +2,10 @@
 Testing Automerge by Main
 %%%%%%%%%%%%%%
 First line
+&&&&&&&&&&&&&&
 int sub (a,b) { return(a-b)}D
 Auto merge test by New
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 Auto test by Main 1st
 Last line Auto merge test
 @@@@@@@@@@@@@
